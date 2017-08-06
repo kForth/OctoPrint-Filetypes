e0cc8380f342dd3e65f7508351d43cb9898c1097
TheLongRunSmoke <thelongrunsmoke@gmail.com>
2017-08-06 14:09:30 +0000

    Bindings in progress

--- octoprint_filetypes/static/js/filetypes.js
+++ octoprint_filetypes/static/js/filetypes.js
@@ -6,17 +6,25 @@
  */
 $(function() {
 
-    alert("Filetypes.js loaded");
-    $('#gcode_upload').attr('accept', ".stl");
+    //$('#gcode_upload').attr('accept', ".stl");
 
     function FiletypesViewModel(parameters) {
         var self = this;
+	self.settings = parameters[0];
 
-        // assign the injected parameters, e.g.:
-        // self.loginStateViewModel = parameters[0];
-        // self.settingsViewModel = parameters[1];
+	self.stl = ko.observable();
 
-        // TODO: Implement your plugin's view model here.
+        self.onBeforeBinding = function() {
+            console.log(self.settings.settings.plugins.filetypes)
+            self.filetypes = self.settings.settings.plugins.filetypes;
+	    var result = [];
+	    if (self.filetypes.stl()) result.push(".stl");
+	    if (self.filetypes.gcode()) result.push(".gcode");
+	    if (self.filetypes.gco()) result.push(".gco");
+            if (self.filetypes.g()) result.push(".g");
+            $('#gcode_upload').attr('accept', result.join(','));	    	     
+        }
+	
     }
 
     // view model class, parameters for constructor, container to bind to
@@ -24,9 +32,9 @@ $(function() {
         FiletypesViewModel,
 
         // e.g. loginStateViewModel, settingsViewModel, ...
-        [ /* "loginStateViewModel", "settingsViewModel" */ ],
+        [ "settingsViewModel"],
 
         // e.g. #settings_plugin_filetypes, #tab_plugin_filetypes, ...
-        [ /* ... */ ]
+        [  ]
     ]);
 });

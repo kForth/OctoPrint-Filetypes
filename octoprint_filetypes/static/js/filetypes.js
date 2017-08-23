00860da09a15dceacc36a1c4f97d9b901d325942
TheLongRunSmoke <thelongrunsmoke@gmail.com>
2017-08-23 03:13:14 +0000

    Some refactor

--- octoprint_filetypes/static/js/filetypes.js
+++ octoprint_filetypes/static/js/filetypes.js
@@ -10,25 +10,30 @@ $(function() {
 	self.settings = parameters[0];
 
 	self.type = {
-	    stl : ko.observable()
-	};
+	        stl : ko.observable(),
+	        gcode : ko.observable(),
+	        gco : ko.observable(),
+	        g : ko.observable()
+	    };
 
-	self.updateTypes = function(){
-	    console.log("Filetypes updated");
-	};
-
-	self.type.stl.subscribe(self.updateTypes);
+	   self.updateTypes = function(){
+	        var keys = Object.keys(self.type);
+	        var result = [];
+	        keys.forEach(function(key){
+	            if (self.type[key]()) result.push("."+key);
+	        });
+	        $('#gcode_upload').attr('accept', result.join(','));
+	    };
 
         self.onBeforeBinding = function() {
-            console.log(self.settings.settings.plugins.filetypes)
-            self.filetypes = self.settings.settings.plugins.filetypes;
-	    self.type.stl(self.filetypes.stl);
-	    var result = [];
-	    if (self.type.stl) result.push(".stl");
-	    if (self.filetypes.gcode()) result.push(".gcode");
-	    if (self.filetypes.gco()) result.push(".gco");
-            if (self.filetypes.g()) result.push(".g");
-            $('#gcode_upload').attr('accept', result.join(','));	    	     
+            var keys = Object.keys(self.type);
+            keys.forEach(function(key) {
+                self.type[key](self.settings.settings.plugins.filetypes[key]());
+            });
+            keys.forEach(function(key) {
+                self.type[key].subscribe(self.updateTypes);
+            });
+		self.updateTypes();		
         }
 	
     }

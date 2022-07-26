78446550a15d9b958490f14470d6f5d7c6f003b4
Kestin Goforth <kgoforth1503@gmail.com>
2022-07-26 09:19:12 -0400

    Add custom extensions and update settings layout

--- octoprint_filetypes/__init__.py
+++ octoprint_filetypes/__init__.py
@@ -5,76 +5,69 @@ import octoprint.plugin
 
 
 class FiletypesPlugin(octoprint.plugin.StartupPlugin,
-					  octoprint.plugin.TemplatePlugin,
-					  octoprint.plugin.SettingsPlugin,
-					  octoprint.plugin.AssetPlugin):
+                      octoprint.plugin.TemplatePlugin,
+                      octoprint.plugin.SettingsPlugin,
+                      octoprint.plugin.AssetPlugin):
 
-	def get_template_configs(self):
-		return [
-			dict(type="settings")
-		]
+    ##~~ SettingsPlugin mixin
+    def get_settings_defaults(self):
+        return {
+            "defaultTypes": [
+                {'extension': 'stl', 'local': True, 'sd': False},
+                {'extension': 'gcode', 'local': True, 'sd': True},
+                {'extension': 'gco', 'local': True, 'sd': True},
+                {'extension': 'g', 'local': True, 'sd': True},
+            ],
+            "customTypes": [],
+        }
 
-	def on_after_startup(self):
-		self._logger.info("Filetypes. (settings: stl=%s, gcode=%s, gco=%s, g=%s)" % (self._settings.get(["stl"]), self._settings.get(["gcode"]), self._settings.get(["gco"]), self._settings.get(["g"])))
+    ##~~ TemplatePlugin
+    def get_template_configs(self):
+        return [
+            {
+                "type": "settings",
+                "name": "Filetypes Plugin",
+                "template": "filetypes_settings.jinja2",
+                "custom_bindings": True,
+            }
+        ]
 
-	##~~ SettingsPlugin mixin
-	def get_settings_defaults(self):
-		"""
-		Return defaults.
-		:return: dict  
-		"""
-		return dict(
-			stl=True,
-			gcode=True,
-			gco=True,
-			g=True
-		)
+    def get_template_vars(self):
+        return dict(
+            defaultTypes=self._settings.get(["defaultTypes"]),
+            customTypes=self._settings.get(["customTypes"])
+        )
 
-	def get_template_vars(self):
-		return dict(
-			stl=self._settings.get(["stl"]),
-			gcode=self._settings.get(["gcode"]),
-			gco=self._settings.get(["gco"]),
-			g=self._settings.get(["g"])
-		)
+    ##~~ AssetPlugin mixin
+    def get_assets(self):
+        return dict(
+            js=["js/filetypes.js"]
+        )
 
-	##~~ AssetPlugin mixin
-	def get_assets(self):
-		# Define your plugin's asset files to automatically include in the
-		# core UI here.
-		return dict(
-			js=["js/filetypes.js"]
-		)
+    ##~~ Softwareupdate hook
+    def get_update_information(self):
+        return dict(
+            filetypes=dict(
+                displayName="Filetypes Plugin",
+                displayVersion=self._plugin_version,
 
-	##~~ Softwareupdate hook
-	def get_update_information(self):
-		# Define the configuration for your plugin to use with the Software Update
-		# Plugin here. See https://github.com/foosel/OctoPrint/wiki/Plugin:-Software-Update
-		# for details.
-		return dict(
-			filetypes=dict(
-				displayName="Filetypes Plugin",
-				displayVersion=self._plugin_version,
+                # version check: github repository
+                type="github_release",
+                user="TheLongRunSmoke",
+                repo="OctoPrint-Filetypes",
+                current=self._plugin_version,
 
-				# version check: github repository
-				type="github_release",
-				user="TheLongRunSmoke",
-				repo="OctoPrint-Filetypes",
-				current=self._plugin_version,
-
-				# update method: pip
-				pip="https://github.com/TheLongRunSmoke/OctoPrint-Filetypes/archive/{target_version}.zip"
-			)
-		)
+                # update method: pip
+                pip="https://github.com/TheLongRunSmoke/OctoPrint-Filetypes/archive/{target_version}.zip"
+            )
+        )
 
 
 __plugin_name__ = "Filetypes Plugin"
-
-def __plugin_load__():
-	global __plugin_implementation__
-	__plugin_implementation__ = FiletypesPlugin()
-
-	global __plugin_hooks__
-	__plugin_hooks__ = {
-		"octoprint.plugin.softwareupdate.check_config": __plugin_implementation__.get_update_information
-	}
+__plugin_version__ = "0.1.1"
+__plugin_description__ = "Select which filetypes can be uploaded to OctoPrint."
+__plugin_pythoncompat__ = ">=2.7,<4"
+__plugin_implementation__ = FiletypesPlugin()
+__plugin_hooks__ = {
+    "octoprint.plugin.softwareupdate.check_config": __plugin_implementation__.get_update_information
+}
--- octoprint_filetypes/static/js/filetypes.js
+++ octoprint_filetypes/static/js/filetypes.js
@@ -2,41 +2,54 @@
  * View model for OctoPrint-Filetypes
  *
  * Author: thelongrunsmoke
+ * Modified by: Kestin Goforth
  * License: AGPLv3
  */
 $(function() {
     function FiletypesViewModel(parameters) {
         var self = this;
-	    self.settings = parameters[0];
+        self.settings = parameters[0];
+        
+        self.defaultTypes = ko.observableArray([]);
+        self.customTypes = ko.observableArray([]);
 
-	    self.type = {
-	        stl : ko.observable(),
-	        gcode : ko.observable(),
-	        gco : ko.observable(),
-	        g : ko.observable()
-	    };
+        self.addFiletype = function () {
+            self.customTypes.push({
+                extension: ko.observable(""),
+                local: ko.observable(true),
+                sd: ko.observable(false)
+            });
+        };
 
-	    self.updateTypes = function(){
-	        var keys = Object.keys(self.type);
-	        var result = [];
-	        keys.forEach(function(key){
-	            if (self.type[key]()) result.push("."+key);
-	        });
-	        $('#gcode_upload').attr('accept', result.join(','));
-	    };
+        self.removeFiletype = function (filetype) {
+            self.customTypes.remove(filetype);
+        };
+        
+        self.updateTypes = function() {
+            var localTypes = [];
+            var sdTypes = [];
+            _.each(self.defaultTypes().concat(self.customTypes()), function(e){
+                var ext = "." + e.extension();
+                if(e.local() && localTypes.indexOf(ext) < 0)
+                    localTypes.push(ext);
+                if(e.sd() && sdTypes.indexOf(ext) < 0)
+                    sdTypes.push(ext);
+            });
 
+            $('#gcode_upload').attr('accept', localTypes.join(','));
+            $('#gcode_upload_sd').attr('accept', sdTypes.join(','));
+        };
+        
         self.onBeforeBinding = function() {
-            var keys = Object.keys(self.type);
-            // Initialize each element in model
-            keys.forEach(function(key) {
-                self.type[key](self.settings.settings.plugins.filetypes[key]());
-            });
-            // Bind subscriber to views.
-            keys.forEach(function(key) {
-                self.type[key].subscribe(self.updateTypes);
-            });
-		    self.updateTypes();
-        }
+            self.defaultTypes(self.settings.settings.plugins.filetypes.defaultTypes());
+            self.customTypes(self.settings.settings.plugins.filetypes.customTypes());
+
+            self.updateTypes();
+        };
+        
+        self.onSettingsBeforeSave = function() {
+            self.updateTypes();
+        };
     }
 
     OCTOPRINT_VIEWMODELS.push([
